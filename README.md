158d8f92e7862a3ea1307d56a1bbaeabd9d83c78
KingJA <kingjavip@gmail.com>
2017-12-12 23:47:12 +0800

    finish

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
  # 图片路径用v-bind
  <img v-bind:src="item.productImage" alt="烟">
+
+ # v-if="!address.isDefault"无效
+ # 处理more的隐藏
+ # 代码自动提示
--- js/address.js
+++ js/address.js
@@ -1,12 +1,21 @@
 new Vue({
     el: ".container",
     data: {
-        addresses: []
+        addresses: [],
+        currentIndex: 0,
+        shippingType: 1,
+        limit: 3
     },
     mounted: function () {
         this.$nextTick(function () {
             this.addressView();
         })
+    },
+    computed: {
+        filterAddress: function () {
+            return this.addresses.slice(0, this.limit);
+        }
+
     },
     methods: {
         addressView: function () {
@@ -14,6 +23,19 @@ new Vue({
             this.$http.get("data/address.json").then(function (response) {
                 _this.addresses = response.body.result;
             })
+        },
+        setDefault: function (addressId) {
+            this.addresses.forEach(function (address, index) {
+                if (address.addressId == addressId) {
+                    address.isDefault = true;
+                } else {
+                    address.isDefault = false;
+                }
+            });
+        },
+        deleteAddress: function (address) {
+            var index = this.addresses.indexOf(address);
+            this.addresses.splice(index, 1);
         }
 
     }
